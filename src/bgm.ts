f29368333fb9a5556864ea8e6c5d015510a78442
Ovler <ovlertheyoung@gmail.com>
2024-07-04 15:07:32 +0800

    Refactor: chunk function

--- src/bgm.ts
+++ src/bgm.ts
@@ -3,14 +3,19 @@
 import { env } from 'hono/adapter';
 import { Env } from './index';
 
-
-export async function getCalander(c: any) {
+async function fetchbgm(url: string, c: any) {
 	const { BANGUMI_API_KEY } = env<{ BANGUMI_API_KEY: string }>(c);
-	const res = await fetch('https://api.bgm.tv/calendar', {
+	const res =  await fetch(url, {
 		headers: {
 			Authorization: `Bearer ${BANGUMI_API_KEY}`,
 		},
 	});
 	let response: JSON = await res.json();
 	return response;
+}
+
+
+export async function getCalander(c: any) {
+	const res = await fetchbgm('https://api.bgm.tv/calendar', c);
+	return res;
 }
