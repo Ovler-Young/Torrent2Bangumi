d4ede2a27c51fe75a4419851508f4ac3ad12ac49
Ovler <ovlertheyoung@gmail.com>
2024-07-04 15:53:07 +0800

    feat: return list

--- src/bgm.ts
+++ src/bgm.ts
@@ -1,21 +1,37 @@
-// this is to communicate with bangumi.tv
-// env BANGUMI_API_KEY
 import { env } from 'hono/adapter';
 import { Env } from './index';
 
-async function fetchbgm(url: string, c: any) {
+async function fetchbgm(url: string, c: any): Promise<any> {
 	const { BANGUMI_API_KEY } = env<{ BANGUMI_API_KEY: string }>(c);
 	const res =  await fetch(url, {
 		headers: {
 			Authorization: `Bearer ${BANGUMI_API_KEY}`,
 		},
 	});
-	let response: JSON = await res.json();
+	let response = await res.json();
 	return response;
 }
 
 
 export async function getCalander(c: any) {
 	const res = await fetchbgm('https://api.bgm.tv/calendar', c);
-	return res;
+	let cal = extractCalendar(res);
+	return cal;
+}
+
+function extractCalendar(calendar: any[]): any[] {
+	const result: any[] = [];
+	for (const day of calendar) {
+		for (const item of day.items) {
+			result.push({
+				air_weekday: item.air_weekday,
+				id: item.id,
+				name: item.name,
+				name_cn: item.name_cn,
+				air_date: item.air_date,
+				type: item.type
+			});
+		}
+	}
+	return result;
 }
