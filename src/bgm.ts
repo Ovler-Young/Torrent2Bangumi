7876f9f27a9b29083101f374a113e60a8512bcb6
Ovler <ovlertheyoung@gmail.com>
2024-07-04 16:12:05 +0800

    chore: add interfaces to avoid any

--- src/bgm.ts
+++ src/bgm.ts
@@ -1,27 +1,91 @@
 import { env } from 'hono/adapter';
 import { Env } from './index';
 
+interface BangumiImage {
+    large: string;
+    common: string;
+    medium: string;
+    small: string;
+    grid: string;
+}
+
+interface BangumiRatingCount {
+    [key: number]: number;
+}
+
+interface BangumiRating {
+    total: number;
+    count: BangumiRatingCount;
+    score: number;
+}
+
+interface BangumiCollection {
+    wish: number;
+    collect: number;
+    doing: number;
+    on_hold: number;
+    dropped: number;
+}
+
+interface BangumiItem {
+    id: number;
+    url: string;
+    type: number;
+    name: string;
+    name_cn: string;
+    summary: string;
+    air_date: string;
+    air_weekday: number;
+    images: BangumiImage;
+    eps: number;
+    eps_count: number;
+    rating: BangumiRating;
+    rank: number;
+    collection: BangumiCollection;
+}
+
+interface BangumiSipleItem {
+	id: number;
+	name: string;
+	name_cn: string;
+	air_date: string;
+	air_weekday: number;
+	type: number;
+}
+
+interface CalendarWeekday {
+    en: string;
+    cn: string;
+    ja: string;
+    id: number;
+}
+
+interface CalendarDay {
+    weekday: CalendarWeekday;
+    items: BangumiItem[];
+}
+
 async function fetchbgm(url: string, c: any): Promise<any> {
-	const { BANGUMI_API_KEY } = env<{ BANGUMI_API_KEY: string }>(c);
-	const res =  await fetch(url, {
-		headers: {
-			Authorization: `Bearer ${BANGUMI_API_KEY}`,
-		},
-	});
-	let response = await res.json();
-	return response;
+    const { BANGUMI_API_KEY } = env<{ BANGUMI_API_KEY: string }>(c);
+    const res = await fetch(url, {
+        headers: {
+            Authorization: `Bearer ${BANGUMI_API_KEY}`,
+        },
+    });
+    let response = await res.json();
+    return response;
 }
 
-export async function getCalendar(c: any): Promise<BangumiItem[]> {
-	const res = await fetchbgm('https://api.bgm.tv/calendar', c);
-	let cal = extractCalendar(res);
-	return cal;
+export async function getCalendar(c: any): Promise<BangumiSipleItem[]> {
+    const res = await fetchbgm('https://api.bgm.tv/calendar', c);
+    let cal = extractCalendar(res);
+    return cal;
 }
 
-function extractCalendar(calendar: any[]): any[] {
-	const result: any[] = [];
-	for (const day of calendar) {
-		for (const item of day.items) {
+function extractCalendar(calendar: CalendarDay[]): BangumiSipleItem[] {
+    const result: BangumiSipleItem[] = [];
+    for (const day of calendar) {
+        for (const item of day.items) {
 			result.push({
 				air_weekday: item.air_weekday,
 				id: item.id,
@@ -31,6 +95,6 @@ function extractCalendar(calendar: any[]): any[] {
 				type: item.type
 			});
 		}
-	}
-	return result;
+    }
+    return result;
 }
